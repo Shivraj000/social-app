5216821d725fa86c02b74f15e52a1060c57cda35
Shivraj <142338267+Shivraj000@users.noreply.github.com>
2026-02-03 17:18:49 +0530

    Filter out current user in FollowersCard
    Refactor FollowersCard component to filter out current user from suggestions.

--- client/src/Components/FollowersCard/FollowersCard.js
+++ client/src/Components/FollowersCard/FollowersCard.js
@@ -4,36 +4,28 @@ import UserFollow from '../UserFollow/UserFollow';
 import { useSelector } from 'react-redux';
 import { getAllUser } from '../../api/UserRequest';
 
-
-
-
 const FollowersCard = () => {
-
   const [persons, setPersons] = useState([]);
   const { user } = useSelector((state) => state.authReducer.authData);
-
 
   useEffect(() => {
     const fetchPersons = async () => {
       const { data } = await getAllUser();
       setPersons(data);
-    }
+    };
     fetchPersons();
-  }, [])
-
-
+  }, []);
 
   return (
     <div className='FollowersCard'>
       <h3>People you may know...</h3>
 
-      {persons.map((person, id) => {
-
-        if (person._id !== user._id) {
-          return <UserFollow person={person} key={id} />
-        }
-
-      })}
+      {persons
+        .filter(person => person._id !== user._id)
+        .map(person => (
+          <UserFollow person={person} key={person._id} />
+        ))
+      }
 
     </div>
   )
